6deadb4e1aa1956ac7f912a6bf95fa92624a6d29
frank <frank@aheadintranet.com>
2022-12-02 21:53:57 +0100

    Day 2 - Part1

---
+++ AdventOfCode/Day2/RockPaperScissors.cs
@@ -0,0 +1,64 @@
+namespace AdventOfCode.Day2;
+
+public class RockPaperScissors : IAdventDay
+{
+    public static string Day => "Day2";
+
+    public static string Run(Context ctx)
+    {
+        (Result, int) Winner(Move opponentMove, Move myMove) =>
+            (opponentMove, myMove) switch
+            {
+                (Move.Rock, Move.Scissors) or (Move.Paper, Move.Rock) or (Move.Scissors, Move.Paper) 
+                    => (Result.Loss, PointsFromMove(myMove)),
+                (Move.Scissors, Move.Rock) or (Move.Rock, Move.Paper) or (Move.Paper, Move.Scissors) 
+                    => (Result.Win, PointsFromMove(myMove)),
+                _ => (Result.Draw, PointsFromMove(myMove))
+            };
+
+        var points = ctx.GetIterator("input.txt")
+            .Select(line => line.Split(" "))
+            .Select(result => Winner(Translate(result[0]), Translate(result[1])))
+            .Select(roundResult => roundResult switch
+            {
+                (Result.Win, var pointsFromRound) => 6 + pointsFromRound,
+                (Result.Loss, var pointsFromRound) => pointsFromRound,
+                (Result.Draw, var pointsFromRound) => 3 + pointsFromRound,
+                _ => throw new ArgumentOutOfRangeException()
+            })
+            .Sum();
+
+
+        return points.ToString();
+    }
+
+    private static Move Translate(string input) => input switch
+    {
+        "A" or "X" => Move.Rock,
+        "B" or "Y" => Move.Paper,
+        "C" or "Z" => Move.Scissors,
+        _ => throw new ArgumentOutOfRangeException(nameof(input), input, null)
+    };
+
+    private static int PointsFromMove(Move move) => move switch
+    {
+        Move.Rock => 1,
+        Move.Paper => 2,
+        Move.Scissors => 3,
+        _ => throw new ArgumentOutOfRangeException(nameof(move), move, null)
+    };
+
+    private enum Move
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    private enum Result
+    {
+        Loss,
+        Win,
+        Draw
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,13 +1,14 @@
 ﻿using AdventOfCode;
 using AdventOfCode.Day1;
+using AdventOfCode.Day2;
 
 static void Run<T>() where T : IAdventDay
 {
-    Console.WriteLine("Day1:");
+    Console.WriteLine($"{T.Day}:");
     var output = T.Run(new Context(T.Day));
     Console.WriteLine(output);
 }
 
 Console.WriteLine("Advent of Code!");
 
-Run<ElveWithMostCalories>();
+Run<RockPaperScissors>();

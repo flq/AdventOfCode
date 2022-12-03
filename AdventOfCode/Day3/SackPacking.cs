b6173f894b10cae72c2f1df4f16f4257a8b8b3a2
frank <frank@aheadintranet.com>
2022-12-03 12:38:19 +0100

    Day 3 - Part 1

---
+++ AdventOfCode/Day3/SackPacking.cs
@@ -0,0 +1,29 @@
+namespace AdventOfCode.Day3;
+
+public abstract class SackPacking : IAdventDay
+{
+    public static string Day => "Day3";
+
+    public static string Run(Context ctx) => ctx.GetInputIterator()
+        .Select(GetPriorityOfDoubleItem)
+        .Sum()
+        .ToString();
+
+    // Use ASCII code of char as numeric value and offset according to requirements
+    private static int Priority(char character) => character - (char.IsUpper(character) ? 38 : 96);
+
+    private static int GetPriorityOfDoubleItem(string sack)
+    {
+        var chars = sack.AsSpan();
+        var firstCompartment = chars[..(chars.Length / 2)];
+        var secondCompartment = chars[(chars.Length / 2)..];
+
+        //A good little O(N^2) but it's December...
+        for (var i = 0; i < firstCompartment.Length; i++)
+        for (var j = 0; j < secondCompartment.Length; j++)
+            if (firstCompartment[i] == secondCompartment[j])
+                return Priority(firstCompartment[i]);
+
+        throw new ArgumentException("No double item in this sack");
+    }
+}

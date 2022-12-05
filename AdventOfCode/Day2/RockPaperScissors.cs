e7d085b226892d4205d56d090877d987a3dd67d3
frank <frank@aheadintranet.com>
2022-12-05 17:30:54 +0100

    Day 2 - Step2

--- AdventOfCode/Day2/RockPaperScissors.cs
+++ AdventOfCode/Day2/RockPaperScissors.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CS8524
 namespace AdventOfCode.Day2;
 
 public abstract class RockPaperScissors : IAdventDay
@@ -9,28 +10,59 @@ public abstract class RockPaperScissors : IAdventDay
         (Result, int) Winner(Move opponentMove, Move myMove) =>
             (opponentMove, myMove) switch
             {
-                (Move.Rock, Move.Scissors) or (Move.Paper, Move.Rock) or (Move.Scissors, Move.Paper) 
+                (Move.Rock, Move.Scissors) or (Move.Paper, Move.Rock) or (Move.Scissors, Move.Paper)
                     => (Result.Loss, PointsFromMove(myMove)),
-                (Move.Scissors, Move.Rock) or (Move.Rock, Move.Paper) or (Move.Paper, Move.Scissors) 
+                (Move.Scissors, Move.Rock) or (Move.Rock, Move.Paper) or (Move.Paper, Move.Scissors)
                     => (Result.Win, PointsFromMove(myMove)),
                 _ => (Result.Draw, PointsFromMove(myMove))
             };
 
-        var points = ctx.GetInputIterator()
+        var pointsStep1 = ctx.GetInputIterator()
             .Select(line => line.Split(" "))
             .Select(result => Winner(Translate(result[0]), Translate(result[1])))
-            .Select(roundResult => roundResult switch
-            {
-                (Result.Win, var pointsFromRound) => 6 + pointsFromRound,
-                (Result.Loss, var pointsFromRound) => pointsFromRound,
-                (Result.Draw, var pointsFromRound) => 3 + pointsFromRound,
-                _ => throw new ArgumentOutOfRangeException()
-            })
+            .Select(PointsCalculator)
             .Sum();
 
-
-        return points.ToString();
+        var pointsStep2 = (from line in ctx.GetInputIterator()
+                let pieces = line.Split(" ")
+                let opponentMove = Translate(pieces[0])
+                let result =
+                    PointsCalculator(Winner(opponentMove, ProperMoveForDesiredOutcome(opponentMove, pieces[1])))
+                select result)
+            .Sum();
+        
+        return $"{pointsStep1.ToString()},{pointsStep2.ToString()}";
     }
+
+    private static Move ProperMoveForDesiredOutcome(Move opponentMove, string desiredOutcome)
+    {
+        return (opponentMove, desiredOutcome) switch
+        {
+            (_, "Y") => opponentMove, // Draw
+            (_, "Z") => opponentMove switch // Win
+            {
+                Move.Paper => Move.Scissors,
+                Move.Scissors => Move.Rock,
+                Move.Rock => Move.Paper
+            },
+            (_, "X") => opponentMove switch // Lose
+            {
+                Move.Paper => Move.Rock,
+                Move.Scissors => Move.Paper,
+                Move.Rock => Move.Scissors
+            },
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    private static int PointsCalculator((Result, int) roundResult) =>
+        roundResult switch
+        {
+            (Result.Win, var pointsFromRound) => 6 + pointsFromRound,
+            (Result.Loss, var pointsFromRound) => pointsFromRound,
+            (Result.Draw, var pointsFromRound) => 3 + pointsFromRound,
+            _ => throw new ArgumentOutOfRangeException()
+        };
 
     private static Move Translate(string input) => input switch
     {

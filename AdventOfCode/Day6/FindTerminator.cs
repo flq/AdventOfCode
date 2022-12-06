2ec3fec894b611e9b9fc5bf74e6ac75fd1426cef
frank <frank@aheadintranet.com>
2022-12-06 20:50:45 +0100

    Day & - parameterized version

--- AdventOfCode/Day6/FindTerminator.cs
+++ AdventOfCode/Day6/FindTerminator.cs
@@ -1,25 +1,28 @@
 namespace AdventOfCode.Day6;
 
-public class FindTerminator : IAdventDay
+public abstract class FindTerminator : IAdventDay
 {
     public static string Day => "Day6";
 
     public static string Run(Context ctx)
     {
-        var messageStream = ctx.GetInputAsMemory();
-        var consumedBytes = 0;
-        while (consumedBytes < messageStream.Length - 4)
+        const int terminatorLength = 14;
+        static bool CheckIfAllDifferent(ReadOnlySpan<byte> bytes)
         {
-            var items = messageStream.Slice(consumedBytes, 4).Span;
-            var allDifferent = items switch
-            {
-                [var one, var two, var three, var four]
-                    when one != two && one != three && one != four &&
-                         two != three && two != four &&
-                         three != four => true,
-                _ => false
-            };
-            if (allDifferent) return (consumedBytes + 4).ToString();
+            for (var i = 0; i < bytes.Length; i++)
+            for (var j = i + 1; j < bytes.Length; j++)
+                if (bytes[i] == bytes[j])
+                    return false;
+            return true;
+        }
+        
+        var messageStream = ctx.GetInputAsMemory();
+        
+        var consumedBytes = 0;
+        while (consumedBytes < messageStream.Length - terminatorLength)
+        {
+            var items = messageStream.Slice(consumedBytes, terminatorLength).Span;
+            if (CheckIfAllDifferent(items)) return (consumedBytes + terminatorLength).ToString();
             consumedBytes++;
         }
 

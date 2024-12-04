bfa5a5871cdc696618df8f7c7aaf1fa57e469243
Frank Quednau <fquednau@gmail.com>
2024-12-04 20:44:58 +0100

    Day4 will have to do with a silver star

---
+++ AdventOfCode/2024/Day4/Challenge.cs
@@ -0,0 +1,57 @@
+namespace AdventOfCode._2024.Day4;
+
+public class Challenge : IAdventDay
+{
+    public static string Day => "Day4";
+
+    public static string Run(Context ctx)
+    {
+        var m = ctx.GetInputAsSingleString();
+        var lineLength = m.Span.IndexOf(Environment.NewLine);
+        Console.WriteLine("lineLength: " + lineLength);
+        var checker = new Checker(lineLength, m);
+
+        var currentIndex = 0;
+        var sum = 0;
+        while (currentIndex < m.Length)
+        {
+            var nextIndex = m.Slice(currentIndex).Span.IndexOf("X");
+            if (nextIndex == -1) break;
+            sum += checker.NumberOfXMases(currentIndex + nextIndex);
+            currentIndex += nextIndex + 1;
+        }
+
+        return sum.ToString();
+    }
+}
+
+public class Checker(int lineLength, ReadOnlyMemory<char> grid)
+{
+    private readonly int phase = lineLength + 1; // +1 from the new-line
+
+    public int NumberOfXMases(int x)
+    {
+        return
+            Check(x, x + 1, x + 2, x + 3) + // Horizontal ->
+            Check(x, x - 1, x - 2, x - 3) + // Horizontal <-
+            Check(x, x - phase, x - 2 * phase, x - 3 * phase) + // Vertical up
+            Check(x, x + phase, x + 2 * phase, x + 3 * phase) + // Vertical down
+            Check(x, x - (phase + 1), x - 2 * (phase + 1), x - 3 * (phase + 1)) + // Diag up left  
+            Check(x, x - (phase - 1), x - 2 * (phase - 1), x - 3 * (phase - 1)) + // Diag up right  
+            Check(x, x + (phase - 1), x + 2 * (phase - 1), x + 3 * (phase - 1)) + // Diag down left  
+            Check(x, x + phase + 1, x + 2 * (phase + 1), x + 3 * (phase + 1)); // Diag down right  
+    }
+
+    private int Check(int xx, int mm, int aa, int ss)
+    {
+        var span = grid.Span;
+        if (xx < 0 || xx >= span.Length) return 0;
+        if (mm < 0 || mm >= span.Length) return 0;
+        if (aa < 0 || aa >= span.Length) return 0;
+        if (ss < 0 || ss >= span.Length) return 0;
+
+        var (x, m, a, s) = (span[xx], span[mm], span[aa], span[ss]);
+
+        return x == 'X' && m == 'M' && a == 'A' && s == 'S' ? 1 : 0;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("Advent of Code!");
 
-Run<AdventOfCode._2024.Day3.Challenge>();
+Run<AdventOfCode._2024.Day4.Challenge>();
 
 return;
 
